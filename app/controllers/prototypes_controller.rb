7ae6968ade50ba8ff6c0401957c46b378e1b3161
takahirohirayama <honobonohinatabokko@gmail.com>
2021-03-03 07:15:27 +0900

    編集画面へ遷移できるように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,7 +26,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless current_user == @prototype.user.id
+    unless current_user.id == @prototype.user.id
       redirect_to action: :index
     end
   end
